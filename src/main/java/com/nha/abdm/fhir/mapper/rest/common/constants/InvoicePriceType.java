d400e74da1ff373370217b7eb152f8404a5abb7f
Venu-Ajitesh <Venu@4444>
2025-08-20 05:18:43 +0530

    Invoice: Updated InvoicePriceType

--- src/main/java/com/nha/abdm/fhir/mapper/rest/common/constants/InvoicePriceType.java
+++ src/main/java/com/nha/abdm/fhir/mapper/rest/common/constants/InvoicePriceType.java
@@ -9,15 +9,36 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum InvoicePriceType {
-  BASE("base"),
-  SURCHARGE("surcharge"),
-  DEDUCTION("deduction"),
-  DISCOUNT("discount"),
-  SGST("SGST"),
-  CGST("CGST"),
-  INFORMATIONAL("informational");
+  BASE("base", "00", "https://nrces.in/ndhm/fhir/r4/CodeSystem/ndhm-price-components", "MRP"),
+  SURCHARGE(
+      "surcharge", "01", "https://nrces.in/ndhm/fhir/r4/CodeSystem/ndhm-price-components", "Rate"),
+  DEDUCTION(
+      "deduction",
+      "02",
+      "https://nrces.in/ndhm/fhir/r4/CodeSystem/ndhm-price-components",
+      "Discount"),
+  DISCOUNT(
+      "discount",
+      "02",
+      "https://nrces.in/ndhm/fhir/r4/CodeSystem/ndhm-price-components",
+      "Discount"),
+  CGST("CGST", "03", "https://nrces.in/ndhm/fhir/r4/CodeSystem/ndhm-price-components", "Tax"),
+  SGST("SGST", "04", "https://nrces.in/ndhm/fhir/r4/CodeSystem/ndhm-price-components", "Tax"),
+  INFORMATIONAL(
+      "informational",
+      "informational",
+      "https://nrces.in/ndhm/fhir/r4/CodeSystem/ndhm-price-components",
+      "informational");
 
-  @JsonValue private final String value;
+  private final String value;
+  private final String code;
+  private final String system;
+  private final String display;
+
+  @JsonValue
+  public String getValue() {
+    return value;
+  }
 
   @JsonCreator
   public static InvoicePriceType fromValue(String value) {
