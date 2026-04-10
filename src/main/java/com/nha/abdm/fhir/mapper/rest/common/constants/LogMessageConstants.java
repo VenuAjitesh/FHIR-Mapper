3a8b397ba5a6a46aa5ec6921c642f7fc5107e41b
Venu-Ajitesh <abdm.yep3@nha.gov.in>
2026-04-10 13:04:18 +0530

    Added constants

--- src/main/java/com/nha/abdm/fhir/mapper/rest/common/constants/LogMessageConstants.java
+++ src/main/java/com/nha/abdm/fhir/mapper/rest/common/constants/LogMessageConstants.java
@@ -2,6 +2,7 @@
 package com.nha.abdm.fhir.mapper.rest.common.constants;
 
 public class LogMessageConstants {
-  public static final String JDBC_EXCEPTION_MESSAGE = " JDBCException Generic SQL Related Error, kindly check logs.";
+  public static final String JDBC_EXCEPTION_MESSAGE =
+      " JDBCException Generic SQL Related Error, kindly check logs.";
   public static final String UNKNOWN_PRODUCT_TYPE = "Unknown product type: ";
 }
