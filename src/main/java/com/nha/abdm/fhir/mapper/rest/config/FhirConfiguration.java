ac88927a4ba152323b1e89747c9deac53ae64f1f
Venu-Ajitesh <abdm.yep3@nha.gov.in>
2026-04-10 14:40:11 +0530

    Added FHIR Exception handler

--- src/main/java/com/nha/abdm/fhir/mapper/rest/config/FhirConfiguration.java
+++ src/main/java/com/nha/abdm/fhir/mapper/rest/config/FhirConfiguration.java
@@ -2,14 +2,22 @@
 package com.nha.abdm.fhir.mapper.rest.config;
 
 import ca.uhn.fhir.context.FhirContext;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class FhirConfiguration {
+public class FhirConfiguration implements WebMvcConfigurer {
 
   @Bean
   public FhirContext fhirContext() {
     return FhirContext.forR4();
+  }
+
+  @Override
+  public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    converters.add(0, new FhirHttpMessageConverter(fhirContext()));
   }
 }
---
+++ src/main/java/com/nha/abdm/fhir/mapper/rest/exceptions/FhirMapperException.java
@@ -0,0 +1,14 @@
+/* (C) 2026 */
+package com.nha.abdm.fhir.mapper.rest.exceptions;
+
+import lombok.Getter;
+
+@Getter
+public class FhirMapperException extends RuntimeException {
+  private final String errorCode;
+
+  public FhirMapperException(String errorCode, String message) {
+    super(message);
+    this.errorCode = errorCode;
+  }
+}
