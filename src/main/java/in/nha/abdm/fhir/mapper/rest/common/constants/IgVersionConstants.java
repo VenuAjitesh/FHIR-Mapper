18fabdc0e64f3ac1e0fee130635beefd8e98fed0
Venu-Ajitesh <abdm.yep3@nha.gov.in>
2026-08-13 04:38:22 +0530

    Upgrade bundled NRCES IG package from 6.5.0 to 7.0.0
    Needed for INPS (Indian Patient Summary), which ships as part of the
    7.0.0 release. Regression pass across existing bundle types in
    progress on this branch before any INPS-specific code is added.

--- src/main/java/in/nha/abdm/fhir/mapper/rest/common/constants/IgVersionConstants.java
+++ src/main/java/in/nha/abdm/fhir/mapper/rest/common/constants/IgVersionConstants.java
@@ -2,6 +2,6 @@
 package in.nha.abdm.fhir.mapper.rest.common.constants;
 
 public class IgVersionConstants {
-  public static final String EXPECTED_NRCES_IG_VERSION = "6.5.0";
+  public static final String EXPECTED_NRCES_IG_VERSION = "7.0.0";
   public static final String NRCES_IG_CANONICAL = "https://nrces.in/ndhm/fhir/r4";
 }
